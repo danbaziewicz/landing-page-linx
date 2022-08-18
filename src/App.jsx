5049e53894c025c6c44c47cdc2076cf517a5a633
danbaziewicz <danxbazi@gmail.com>
2022-08-18 16:19:55 -0300

    feat: criação de arquivos base projeto.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Description from './components/Description/Description'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+function App() {
+
+
+  return (
+    <div>
+      <Header/>
+
+      <Description/>
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
