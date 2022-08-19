672918005b831ae80ec33d8d517cacca7bad7f69
danbaziewicz <danxbazi@gmail.com>
2022-08-19 18:39:28 -0300

    feat: implementado footer.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import S from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={S.footer}>
+        <p className={S.footerText}>Testando suas habilidades em HTML, CSS e JS. <br/>Linx Impulse<br/> 2019</p>
+    </footer>
+  )
+}
+
+export default Footer
