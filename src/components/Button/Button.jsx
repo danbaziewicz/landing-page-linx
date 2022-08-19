e07fa864cb275975025288d2a422f02bac49bbab
danbaziewicz <danxbazi@gmail.com>
2022-08-19 18:44:35 -0300

    feat: criado botão para card.

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import S from './Button.module.css'
+
+const Button = () => {
+  return (
+    <button className={S.btnCard}>Comprar</button>
+  )
+}
+
+export default Button
