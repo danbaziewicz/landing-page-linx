4b90960dc49e694076ae374b1b761591bf51acd7
danbaziewicz <danxbazi@gmail.com>
2022-08-19 22:28:39 -0300

    feat: implementada requisição da api.

--- src/components/Produtos/Produtos.jsx
+++ src/components/Produtos/Produtos.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Buttonmais from '../Buttonmais/Buttonmais'
 import Cardproduto from '../Cardproduto/Cardproduto'
 import S from './Produtos.module.css'
 
 const Produtos = () => {
+  const [infos, setInfo] = useState([])
+  const [pagina, setPagina] = useState(1)
+
+  const handleReq = async () => {
+    const resp = await fetch(
+      `https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=${pagina}`
+    );
+    const json = await resp.json();
+    setInfo([...infos,...json.products])
+  }
+
+  const nextPage = () => {
+    setPagina(pagina+1)
+  }
+
+  useEffect(()=>{
+    handleReq();
+  }, [pagina])
+
   return (
     <div>
-      <div className={S.produtos}>
-        <h3 className={S.tituloProdutos}>Sua seleção espcial</h3>
-      </div>
       <div className={S.divProdutos}>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
-        <Cardproduto/>
+        {infos.length>0 && infos.map((item, index) => {
+          return (
+            <Cardproduto
+            img={item.image}
+            nome={item.name}
+            desc={item.description}
+            precoAnt={item.oldPrice}
+            preco={item.price}
+            parc={item.installments.count}
+            precoParc={item.installments.value}/>
+          )
+        })}
       </div>
+      <Buttonmais onClick={nextPage}/>
     </div>
   )
 }
