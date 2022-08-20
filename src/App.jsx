e4cd68c067cce31c528dbfc1e7d3513c674f3cc7
danbaziewicz <danxbazi@gmail.com>
2022-08-20 13:33:56 -0300

    feat: adicionado novo componente ao app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Produtos from './components/Produtos/Produtos'
 import Section from './components/Section/Section'
+import Section1 from './components/Section1/Section1'
 
 function App() {
 
@@ -11,8 +12,8 @@ function App() {
     <div>
       <Header/>
       <Section/>
+      <Section1/>
       <Produtos/>
-
       <Footer/>
     </div>
   )
