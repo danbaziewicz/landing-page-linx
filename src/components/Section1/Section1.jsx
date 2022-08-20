c9243676540563b4de97c1d4817eef9a0ff18254
danbaziewicz <danxbazi@gmail.com>
2022-08-20 13:30:47 -0300

    feat: separando elementos do componente produto.

---
+++ src/components/Section1/Section1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import S from './Section1.module.css'
+
+const Section1 = () => {
+  return (
+    <div className={S.produtos}>
+    <h3 className={S.tituloProdutos}>Sua seleção espcial</h3>
+  </div>
+  )
+}
+
+export default Section1
