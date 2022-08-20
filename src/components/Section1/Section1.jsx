e0b60fd0ae76d38b9d799068c0e961478e5f6ea9
danbaziewicz <danxbazi@gmail.com>
2022-08-20 14:46:29 -0300

    feat: adicionada props para descrição da tag.

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import S from './Section1.module.css'
 
-const Section1 = () => {
+const Section1 = ({textoSection}) => {
   return (
     <div className={S.produtos}>
-    <h3 className={S.tituloProdutos}>Sua seleção espcial</h3>
+    <h3 className={S.tituloProdutos}>{textoSection}</h3>
   </div>
   )
 }
