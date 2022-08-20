8c7d3c1a4025cfb8a65e1707d011aafd3c0c5895
danbaziewicz <danxbazi@gmail.com>
2022-08-19 22:27:56 -0300

    feat: ajustes de card produto.

--- src/components/Cardproduto/Cardproduto.jsx
+++ src/components/Cardproduto/Cardproduto.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Button from '../Button/Button'
 import S from './Cardproduto.module.css'
 
-const Cardproduto = () => {
+const Cardproduto = ({img, nome, desc, precoAnt, preco, parc, precoParc}) => {
   return (
     <div className={S.card}>
-        <div className={S.imgCard}></div>
-            <p className={S.nomeProd}>Nome do produto</p>
-            <p className={S.descProd}>Descrição do produto um pouco maior, com duas linhas ou três que explica melhor do que se trata.</p>
-            <p className={S.precoAntigo}>De: R$23,99</p>
-            <p className={S.preco}>Por: R$19,99</p>
-            <p className={S.precoParc}>ou 2x de R$9,99</p>
+        <div className={S.imgCard}>
+        <img src={img}  alt="produto" />
+        </div>
+            <p className={S.nomeProd}>{nome}</p>
+            <p className={S.descProd}>{desc}</p>
+            <p className={S.precoAntigo}>De: R${precoAnt},99</p>
+            <p className={S.preco}>Por: R${preco},99</p>
+            <p className={S.precoParc}>ou {parc}x de R${precoParc},99</p>
             <Button/>
     </div>
   )
